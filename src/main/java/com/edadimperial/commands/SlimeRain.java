50b19d38ec419d67a29e4ddacb05d1b862c142b9
Kaidenleaf <129576524+Kaidenleaf@users.noreply.github.com>
2023-07-04 12:57:55 -0600

    update: organize code

--- src/main/java/com/edadimperial/commands/SlimeRain.java
+++ src/main/java/com/edadimperial/commands/SlimeRain.java
@@ -1,95 +1,30 @@
 package com.edadimperial.commands;
 
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.Location;
-import org.bukkit.World;
+import com.edadimperial.SlimeRainEvent;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
-
 import org.bukkit.plugin.Plugin;
-import org.bukkit.scheduler.BukkitRunnable;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 public class SlimeRain implements CommandExecutor {
     Plugin instance;
+
     public SlimeRain(Plugin plugin) {
         instance = plugin;
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-
-        return startSlimeRain();
-    }
-
-    private boolean startSlimeRain() {
-        List<Player> players = new ArrayList<>(Bukkit.getServer().getOnlinePlayers());
-        for (Player player : players) {
-            player.sendMessage(ChatColor.translateAlternateColorCodes('&',"&a¡Llueven Slimes!"));
+        if (!(sender instanceof Player)) {
+            return true;
         }
 
-        World world = instance.getServer().getWorld("world");
-
-        int duration = 60;
-        int interval = 5;
-        new BukkitRunnable() {
-            int count = 0;
-            public void run() {
-                count++;
-                List<Player> currentPlayers = new ArrayList<>(Bukkit.getServer().getOnlinePlayers());
-
-                if (world != null && !world.hasStorm()) {
-                    world.setStorm(true);
-                }
-
-                if (count >= duration/interval) {
-                    for (Player player : currentPlayers) {
-                        player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&aSlimes han dejado de caer del cielo"));
-                    }
-                    if (world != null) {
-                        world.setStorm(false);
-                    }
-                    cancel();
-                }
-
-                Collections.shuffle(currentPlayers);
-
-                for (Player currentPlayer : currentPlayers) {
-                    Location loc = currentPlayer.getLocation();
-                    World pWorld = currentPlayer.getWorld();
-
-                    if (pWorld.getName().equals("world")) {
-                        int x = (int) (loc.getX() + (Math.random() * 20));
-                        int y = (int) (loc.getY() + 100);
-                        int z = (int) (loc.getZ() + (Math.random() * 20));
-                        Location spawnLoc = new Location(pWorld, x, y, z);
-                        Entity slime = pWorld.spawnEntity(spawnLoc, EntityType.SLIME);
-                        slime.setPersistent(true);
-                        slime.setCustomName("Slime");
-
-                        // Schedule the entity to be removed after a certain amount of time
-                        Bukkit.getScheduler().runTaskLater(instance, new Runnable() {
-                            public void run() {
-                                slime.remove();
-                            }
-                        }, 20 * 60); // Remove the entity after 60 seconds
-
-
-                    }
-                }
-
-
-            }
-        }.runTaskTimer(instance, 0,interval * 20);
+        if (args[0].equalsIgnoreCase("start")) {
+            SlimeRainEvent rain = new SlimeRainEvent(instance);
+            rain.startRain();
+            return true;
+        }
 
         return true;
     }
